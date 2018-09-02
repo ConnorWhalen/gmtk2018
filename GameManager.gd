be323c7d3333c7899ebba4ac994bc00227a45312
Jimmy <phantommbot@gmail.com>
2018-09-02 17:32:22 -0230

    please dont break

--- GameManager.gd
+++ GameManager.gd
@@ -8,7 +8,11 @@ export(PackedScene) var initialScene
 
 onready var currentScene = initialScene.instance()
 onready var controlsScene = preload("res://ControlsExplanation.tscn").instance()
+onready var titleScene = preload("res://TitleScreen.tscn")
 onready var gameScene = preload("res://stage.tscn")
+
+var currentStage = 1;
+var currentStageString = "res://"+String(currentStage)+".stage"
 
 func _ready():
 	# Called when the node is added to the scene for the first time.
@@ -28,23 +32,38 @@ func on_pass_title():
 func on_pass_controls():
 	self.remove_child(currentScene)
 	currentScene = gameScene.instance()
-	currentScene.stageToLoad = "res://1.stage"
+	currentScene.stageToLoad = currentStageString
 	self.add_child(currentScene)
-	currentScene.connect("stage_lose", self, "on_stage_lose")
-	currentScene.connect("stage_win", self, "on_stage_win")
-
-func on_game_over():
-	currentScene.get_node('song').stop()
-	emit_signal("miss_sample")
+	reconnectStageSignals()
 
 func on_stage_lose():
 	self.remove_child(currentScene)
 	currentScene = gameScene.instance()
+	reconnectStageSignals()
 	self.add_child(currentScene)
 	
 func on_stage_win():
+	self.remove_child(currentScene)
+	var nextScene = gameScene
+	if currentStage == 3:
+		currentStage = 1
+		nextScene = titleScene
+		currentScene = nextScene.instance()
+		currentScene.connect("buttonPressed", self, "on_pass_title")
+	else:
+		currentScene = nextScene.instance()
+		currentStage += 1
+		currentStageString = "res://"+String(currentStage)+".stage"
+		currentScene.stageToLoad = currentStageString
+		reconnectStageSignals()
+	self.add_child(currentScene)
 	pass
 
+func reconnectStageSignals():
+	currentScene.connect("stage_lose", self, "on_stage_lose")
+	currentScene.connect("stage_win", self, "on_stage_win")
+	pass
+	
 #func _process(delta):
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
